e2d3a0213dbb41b4e29879cc28cbed6b64209233
Loke Bendtsen <lokebendtsen@gmail.com>
2023-05-31 12:09:39 +0200

    changes 31-05

--- WinLinuxApp/Program.cs
+++ WinLinuxApp/Program.cs
@@ -6,36 +6,39 @@ namespace WinLinuxApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please choose 1 to make a new file or 2 to check path");
+            Console.WriteLine($"Your user folder is: {Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}");
+            #region opgave 2
+            //Console.WriteLine("Please choose 1 to make a new file or 2 to check path");
 
-            string? choice = Console.ReadLine();
+            //string? choice = Console.ReadLine();
 
-            if (choice == "1")
-            {
-                Console.Write("Input file name: ");
-                string nameOfFile = Console.ReadLine();
-                Console.Write("Input text to file:");
-                string textToFile = Console.ReadLine();
-                MakeFolder fh = new(nameOfFile + ".txt");
-                try
-                {
-                    fh.CreateFile(textToFile);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                
-            }
-            else if (choice == "2")
-            {
-                Console.WriteLine($"Your user folder is: {Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}");
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("Incorrect input");
-            }
+            //if (choice == "1")
+            //{
+            //    Console.Write("Input file name: ");
+            //    string nameOfFile = Console.ReadLine();
+            //    Console.Write("Input text to file:");
+            //    string textToFile = Console.ReadLine();
+            //    MakeFolder fh = new(nameOfFile + ".txt");
+            //    try
+            //    {
+            //        fh.CreateFile(textToFile);
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine(e.Message);
+            //    }
+
+            //}
+            //else if (choice == "2")
+            //{
+            //    Console.WriteLine($"Your user folder is: {Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}");
+            //    Console.ReadKey();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Incorrect input");
+            //}
+            #endregion
 
             int x = 1;
             while (true)
